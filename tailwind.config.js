ca689d62c48159ff7ca14908cd155c9a67dd2cd5
arkontemx <arkonte.mx@outlook.com>
2024-03-11 21:42:07 -0600

    solución a problema con tailwindcss y clases dinamicas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
+/**
+ * ALGUNOS COMPONENTES APLICAN CLASSES DE FORMA DINÁMICA
+ * ESTO PROVOCA QUE TAILWIND NO ENCUENTRE DICHAS CLASES, Y
+ * POR LO TANTO NO GENERE EL CÓDIGO CSS REQUERIDO.
+ *
+ * PARA FORZAR QUE DICHAS CLASSES ESTÉN PRESENTES
+ * EN TIEMPO DE EJECUCIÓN, DEBEN AGREGARSE AL SAFELIST
+ *
+ * https://tailwindcss.com/docs/content-configuration#dynamic-class-names
+ *
+ */
 module.exports = {
   content: ['./src/**/*.tsx'],
+  safelist: [
+    {
+      pattern: /text-(emerald|teal|slate)-(300|600|800)/,
+    },
+  ],
   theme: {},
   variants: {},
   plugins: [],
